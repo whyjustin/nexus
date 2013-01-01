a92670b56fba9e6477e130eaaaef8023cdd3460f
whyjustin <whyjustin@yahoo.com>
2012-12-31 15:36:48 -1000

    My First Plugin setup

---
+++ nexus/plugins/nexus-my-first-plugin/src/main/java/org/nexus/plugin/VirusScanner.java
@@ -0,0 +1,13 @@
+package org.nexus.my.first.plugin;
+
+import javax.inject.Singleton;
+
+import org.sonatype.nexus.proxy.item.StorageFileItem;
+import org.sonatype.plugin.Managed;
+
+@Managed
+@Singleton
+public interface VirusScanner
+{
+    boolean hasVirus( StorageFileItem file );
+}
